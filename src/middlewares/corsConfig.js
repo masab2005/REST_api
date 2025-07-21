d446114b52b5d696ca741918e54e4c064d28c151
masab2005 <muhammad.masab05@gmail.com>
2025-07-21 14:15:17 +0500

    tweaks

--- src/middlewares/corsConfig.js
+++ src/middlewares/corsConfig.js
@@ -1,9 +1,9 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
-const allowed_Origins = process.env.ALLOWED_ORIGINS.split(',');
-const allowed_Headers = process.env.ALLOWED_HEADERS.split(',');
-const allowed_Methods = process.env.ALLOWED_METHODS.split(',');  
+const allowed_Origins = process.env.ALLOWED_ORIGINS? process.env.ALLOWED_ORIGINS.split(',') : [];
+const allowed_Headers = process.env.ALLOWED_HEADERS? process.env.ALLOWED_HEADERS.split(',') : [];
+const allowed_Methods = process.env.ALLOWED_METHODS? process.env.ALLOWED_METHODS.split(',') : [];  
 const CREDENTIALS = process.env.CREDENTIALS === 'true';
 
 export default cors({
